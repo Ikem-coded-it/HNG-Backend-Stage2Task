7746b138e5c66f462304fa07857599ddaf52e38f
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-11-05 17:00:54 +0100

    added cors and dotenv

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const operationRouter = require("./routes/operation.route");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors")
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/operation", operationRouter);
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
 })
