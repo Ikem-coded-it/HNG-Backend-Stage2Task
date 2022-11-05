c857fb94ed992b3bbf4158ae1cced20df97b8662
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-11-05 17:44:12 +0100

    small tweaks

--- routes/operation.route.js
+++ routes/operation.route.js
@@ -18,15 +18,17 @@ router.route("/").post(async(req, res) => {
 
 
 const arithmetics = (op, x, y) => {
-    if (op == "addition") {
+    if (op == "addition" || op == "+") {
         const result = x + y;
         return result;
-    } else if (op == "subtraction") {
+    } else if (op == "subtraction" || op == "-") {
         const result = x - y;
         return result;
-    } else {
+    } else if (op == "multiplication" || op == "*") {
         const result = x * y;
         return result;
+    } else {
+        return "Invalid Operation type."
     }
 }
 
